34b5863a79bb2a0c766b95f1ad3313010f01f813
interfacekun <qq627795061@outlook.com>
2024-05-14 15:52:01 +0800

    可以了

--- test/stage/main_test.go
+++ test/stage/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"testing"
 
+	"github.com/cloudwego/hertz/pkg/common/hlog"
 	md "github.com/xxjwxc/gormt/model"
 	"gorm.io/gorm"
 )
@@ -10,6 +11,12 @@ import (
 func TestMiniGameLog(t *testing.T) {
 	mgl := &md.Minigamelog{Nickname: "ikun"}
 	db := OpenDb()
-	stmt := db.Session(&gorm.Session{DryRun: true}).Create(mgl).Statement
-	t.Logf("test mini gameLog sql:\n%s\n", stmt.SQL.String())
+	// stmt := db.Session(&gorm.Session{DryRun: true}).Create(mgl).Statement
+	// t.Logf("test mini gameLog sql:\n%s\n", stmt.SQL.String())
+	sql := db.ToSQL(func(tx *gorm.DB) *gorm.DB {
+		return tx.Create(mgl)
+	})
+
+	hlog.Infof("test mini gameLog sql:\n%s\n", sql)
+
 }
